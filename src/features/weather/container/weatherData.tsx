b3bffcbdd76fa88201af197e55bf7280fb0c6de2
janvi-scaletech <janvi.kalavadiya@scaletech.xyz>
2023-06-05 08:43:41 +0530

    dynamic data set

--- src/features/weather/container/weatherData.tsx
+++ src/features/weather/container/weatherData.tsx
@@ -10,28 +10,28 @@ import { CloudIcon, SunriseIcon, SunsetIcon, WindIcon } from 'shared/components/
 const WeatherData = () => {
 	const [lat, setLat] = useState(0);
 	const [long, setLong] = useState(0);
-	// const [weatherData, setWeatherData] = useState<any>([]);
+	const [weatherData, setWeatherData] = useState<any>([]);
 	const [cityName, setCityName] = useState<any>(null);
 	const [search, setSearch] = useState('surat');
 
 	const fetchWeatherAPI = () => {
-		// lat &&
-		// 	long > 0 &&
-		// fetch(
-		// 	`${API_CONFIG.baseUrl}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-		// )
-		fetch(`${API_CONFIG.baseUrl}/weather?q=${search}&APPID=${process.env.REACT_APP_API_KEY}`)
-			.then((res) => res.json())
-			.then((result) => {
-				// setWeatherData(result);
-				setCityName(result);
-				console.log(result);
-			})
-			.catch((err: Error) => {
-				// setCityName(null);
-				console.log('err', err);
-				// console.error('Error', err);
-			});
+		lat &&
+			long > 0 &&
+			fetch(
+				`${API_CONFIG.baseUrl}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+			)
+				// fetch(`${API_CONFIG.baseUrl}/weather?q=${search}&APPID=${process.env.REACT_APP_API_KEY}`)
+				.then((res) => res.json())
+				.then((result) => {
+					setWeatherData(result);
+					// setCityName(result);
+					console.log(result);
+				})
+				.catch((err: Error) => {
+					// setCityName(null);
+					console.log('err', err);
+					// console.error('Error', err);
+				});
 	};
 
 	const handleSearch = debounce((value: string) => setSearch(value));
@@ -42,22 +42,11 @@ const WeatherData = () => {
 			setLong(position.coords.longitude);
 		});
 
-		search && fetchWeatherAPI();
-	}, [search]);
+		fetchWeatherAPI();
+	}, [lat, long]);
 
 	return (
 		<div>
-			{/* <h1>{weatherData && weatherData.name}</h1>
-			<p>Temp: {weatherData && weatherData.main.temp}</p>
-			<p>Description :{weatherData.weather[0].description}</p>
-			<p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-			<p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-			<p>Humidity: {weatherData.main.humidity} %</p>
-
-			<p>Day: {moment().format('dddd')}</p>
-			<p>Date: {moment().format('LL')}</p>
-			<p>visibility :{weatherData.visibility}</p> */}
-
 			{/* <h3>{search}</h3>
 			<p>{cityName && cityName.main?.temp}</p>
 			{!isEmpty(cityName) && (
@@ -76,8 +65,13 @@ const WeatherData = () => {
 				<div className='weather-info-wrapper flex'>
 					<div className='weather-description'>
 						<div>
-							<h3 className='font--extra-bold font-size--26'>January 2022</h3>
-							<p className='date-info '>Thursday , Jan 4 2022</p>
+							<h3 className='font--extra-bold font-size--26'>
+								{moment().format('MMMM')} {moment().format('YYYY')}
+							</h3>
+							<p className='date-info '>
+								{' '}
+								{moment().format('dddd')} {moment().format('LL')}
+							</p>
 						</div>
 						<hr className='mt--20 mb--20' />
 						<h3 className='font-size--lg mb--30 font--medium'>Today overview</h3>
@@ -88,7 +82,7 @@ const WeatherData = () => {
 								</div>
 								<div>
 									<p className='date-info pt--10'>Wind Speed</p>
-									<h3 className='font-size--26 font--semi-bold'>12 KM /h</h3>
+									<h3 className='font-size--26 font--semi-bold'>{weatherData.wind.speed}Km /h</h3>
 								</div>
 							</div>
 							<div className='wind-information-wrapper mr--20 mb--20 flex'>
@@ -97,7 +91,7 @@ const WeatherData = () => {
 								</div>
 								<div>
 									<p className='date-info pt--10'>Feels Like</p>
-									<h3 className='font-size--26 font--semi-bold'>38</h3>
+									<h3 className='font-size--26 font--semi-bold'>{weatherData.main.feels_like}</h3>
 								</div>
 							</div>
 						</div>
@@ -108,7 +102,7 @@ const WeatherData = () => {
 								</div>
 								<div>
 									<p className='date-info pt--10'>Humidity</p>
-									<h3 className='font-size--26 font--semi-bold'>63%</h3>
+									<h3 className='font-size--26 font--semi-bold'>{weatherData.main.humidity}%</h3>
 								</div>
 							</div>
 							<div className='wind-information-wrapper mr--20 mb--20 flex'>
@@ -128,16 +122,16 @@ const WeatherData = () => {
 					<div className='weather-info '>
 						<div className='pt--15 flex justify-content--between'>
 							<div>
-								<h2 className='font-size--26 font--medium'>Surat</h2>
+								<h2 className='font-size--26 font--medium'>{weatherData.name}</h2>
 								<p>India</p>
 							</div>
-							<h3 className='font-size--lg font--medium pt--40'>8:54 AM</h3>
+							<h3 className='font-size--lg font--medium pt--40'>{weatherData.timezone}</h3>
 						</div>
 						<div className='mt--30'>
 							<CloudIcon className='' />
 							<div className='flex align-items--center justify-content--between'>
-								<h3>20 C</h3>
-								<p>Cloudy</p>
+								<h3>{weatherData.main.temp}</h3>
+								<p>{weatherData.weather[0].description}</p>
 							</div>
 							<hr />
 						</div>
@@ -148,7 +142,7 @@ const WeatherData = () => {
 									<SunriseIcon className='sunrise-icon' />
 									<div>
 										<p>Sunrise</p>
-										<p>4:20 AM</p>
+										<p>{new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
 									</div>
 									<p> 4 hour ago</p>
 								</div>
@@ -156,7 +150,7 @@ const WeatherData = () => {
 									<SunsetIcon className='sunrise-icon' />
 									<div>
 										<p>Sunset</p>
-										<p>5:50 PM</p>
+										<p>{new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
 									</div>
 									<p> in 9 hours </p>
 								</div>
